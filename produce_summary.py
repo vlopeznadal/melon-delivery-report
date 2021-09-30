652a4281548f7f6846bae75594334487159f4499
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-30 08:38:58 -0500

    Added comments explaining lines of code

--- produce_summary.py
+++ produce_summary.py
@@ -1,19 +1,19 @@
-def produce_summary():
-    day_count = 1
-    file_count = 19
-    while file_count < 22 and day_count < 4:
-        the_file = open("um-deliveries-201405" + str(file_count) + ".txt")
-        file_count += 1
-        print("Day " + str(day_count))
-        day_count += 1
-        for line in the_file:
-            line = line.rstrip()
-            words = line.split('|')
+def produce_summary(): # function to print daily produce summary from files
+    day_count = 1 # day to start count on
+    file_count = 19 # file to start count on
+    while file_count < 22 and day_count < 4: # while the file count is less than 22 and the day count is less than 4 do something
+        the_file = open("um-deliveries-201405" + str(file_count) + ".txt") # open file that includes file_count variable
+        file_count += 1 # increase variable counter by 1
+        print("Day " + str(day_count)) # print day number
+        day_count += 1 # increase variable counter by 1
+        for line in the_file: # for each line in file
+            line = line.rstrip() # remove white space from line
+            words = line.split('|') # split the line by delimiter
 
-            melon = words[0]
-            count = words[1]
-            amount = words[2]
-            print(f"Delivered {count} {melon}s for total of ${amount}")
-    the_file.close()
+            melon = words[0] # store words at index 0 in variable melon
+            count = words[1] # store words at index 1 in variable count
+            amount = words[2] # store words at index 2 in variable amount
+            print(f"Delivered {count} {melon}s for total of ${amount}") # print summary line containing variables
+    the_file.close() # close the file
 
-produce_summary()
+produce_summary() # call function
