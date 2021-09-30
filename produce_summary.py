ca7b5aee9bbdb8478c20fb90d883d2ddc89b28b0
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-30 08:29:45 -0500

    Refactored code into single function

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,19 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def produce_summary():
+    day_count = 1
+    file_count = 19
+    while file_count < 22 and day_count < 4:
+        the_file = open("um-deliveries-201405" + str(file_count) + ".txt")
+        file_count += 1
+        print("Day " + str(day_count))
+        day_count += 1
+        for line in the_file:
+            line = line.rstrip()
+            words = line.split('|')
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+            melon = words[0]
+            count = words[1]
+            amount = words[2]
+            print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+produce_summary()
